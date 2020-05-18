264dd2aa40fff66d4c67e2ed9a226d0b749844d5
meloegel <meloegel@gmail.com>
2020-05-18 18:46:56 -0400

    completed

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+
+
 
 const MovieList = props => {
   return (
@@ -14,15 +17,16 @@ function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
     <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
+      <Link to={`/movies/${movie.id}`}>
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
+        <h3>Actors</h3>
+      </Link>
       {stars.map(star => (
         <div key={star} className="movie-star">
           {star}
